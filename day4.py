a96d868b3b1be791dfbd31227eccce76e85f42f2
karishma0806 <70712631+karishma0806@users.noreply.github.com>
2020-09-08 12:54:47 +0530

    Add files via upload
    assignment

---
+++ day4.py
@@ -0,0 +1,25 @@
+#AMSTRONG
+l=int(input("enter a number"))
+v=int(input("enter a number"))
+for  num in range(l,v+1):
+    t=num
+    sum=0
+    while t >0:
+        x=t%10
+        c=x*x*x
+        sum+=c
+        t=t//10
+    if (num == sum):
+        print(sum)
+        
+
+
+            
+                
+                
+            
+            
+    
+    
+
+
